a01e076cac7cc4668f91aacd6acbe483b553afdc
Carolyn Ruoyin Liu <crliu@linux2.cs.uchicago.edu>
2022-02-11 21:05:10 -0600

    test

--- test.py
+++ test.py
@@ -5,15 +5,15 @@ client = tweepy.Client(bearer_token='AAAAAAAAAAAAAAAAAAAAAPrIYwEAAAAAjiWmjHoKgV4
 # Replace with your own search query
 '''
 # covid query
-query = 'covid19 OR covid OR covid-19 OR vaccine OR vaccination OR omicron OR booster\
+query = '(covid19 OR covid OR covid-19 OR vaccine OR vaccination OR omicron OR booster\
 OR mask OR mandate OR lockdown OR death rate OR travel restriction OR total infections OR\
-breakthrough infections OR social distancing OR quarantine OR isolation OR pandemic OR shutdown\
+breakthrough infections OR social distancing OR quarantine OR isolation OR pandemic OR shutdown)\
 lang:en -is:retweet'
 '''
 
 # us-china query
 query = 'China (Trump OR Biden OR trade war OR Xi Jinping OR Eileen Gu OR Taiwan\
-OR Hong Kong OR CCP OR TikTok OR Huawei OR tariffs OR human rights lang:en -is:retweet)'
+OR Hong Kong OR CCP OR TikTok OR Huawei OR tariffs OR human rights) lang:en -is:retweet'
 
 file_name = 'tweets.csv'
 
@@ -21,5 +21,5 @@ with open(file_name, 'w') as f:
     writer = csv.writer(f)
     for tweet in tweepy.Paginator(client.search_recent_tweets, query=query,
             tweet_fields=['text', 'created_at', 'id', 'public_metrics'],
-            max_results=100).flatten(limit=10000):
+            max_results=100).flatten(limit=1000):
         writer.writerow([tweet.created_at, tweet.author_id, tweet.id, tweet.public_metrics, tweet.text.encode('utf-8')])
