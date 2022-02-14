a0e702fac0410cbbd690de8702c3de34116cdee8
Carolyn Ruoyin Liu <crliu@linux2.cs.uchicago.edu>
2022-02-14 11:36:03 -0600

    2/14 morning pull

--- tweet_pull_cl.py
+++ tweet_pull_cl.py
@@ -8,7 +8,7 @@ query = '(covid19 OR covid OR covid-19 OR vaccine OR vaccination OR omicron OR b
 OR mask OR mandate OR lockdown OR death rate OR travel restriction OR total infections OR\
 breakthrough infections OR social distancing OR quarantine OR isolation OR pandemic OR shutdown)\
 lang:en -is:retweet'
-file_name = 'data/tweets_covid_021222.csv'
+file_name = 'data/tweets_covid_021422.csv'
 
 
 '''
@@ -20,10 +20,10 @@ OR Ned Price)) lang:en -is:retweet'
 file_name = 'data/tweets_china_021222.csv'
 '''
 
-#with open(file_name, 'w') as f:
-with open(file_name, 'a+') as f:
+with open(file_name, 'w') as f:
+#with open(file_name, 'a+') as f:
     writer = csv.writer(f)
     for tweet in tweepy.Paginator(client.search_recent_tweets, query=query,
             tweet_fields=['text', 'created_at', 'author_id', 'id', 'public_metrics'],
-            max_results=100).flatten(limit=50000):
+            max_results=100).flatten(limit=100000):
         writer.writerow([tweet.created_at, tweet.author_id, tweet.id, tweet.public_metrics, tweet.text.encode('utf-8')])
