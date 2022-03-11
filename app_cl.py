db4efae5252c30be4d62b3cd95e5177ef3bb0d1f
Carolyn Liu <carolynliu@Carolyns-MacBook-Pro.local>
2022-03-11 11:14:19 -0600

    updated output

--- app_cl.py
+++ app_cl.py
@@ -12,17 +12,27 @@ app = dash.Dash()
 
 all_options = {
     'linear': ['Information Technology', 'Financials'],
-    'logit': ['All Sectors']
+    'logit': ['All Sectors'],
+    'knn': ['All Sectors'],
+    'svm': ['All Sectors']
 }
 
 df = pd.read_csv('fake_output.csv')
+tr = pd.read_csv('fake_training_output.csv')
 
 app.layout = html.Div([
         html.Div([
             html.H1("Can Moods on Twitter Predict Stock Price Movements?"),
-            #html.P("ABOUT PARAGRAPH")
+            html.P('Stock prices are often influenced by the public attitude '
+                'towards the market. Psychological research shows that emotions play a '
+                'significant role in human decision-making. Similarly, behavioral finance research '
+                "shows that financial decisions are influenced by people's emotions and moods. "),
+            html.P("Twitter is a popular place for users to express their moods and sentiments "
+                "towards a variety of topics. We looked into how the polarity of "
+                "sentiments in tweets on COVID-19 and US-China relations are related to "
+                "the daily movements of stock prices!") 
                 ], 
-            style = {'padding' : '30px' , 'backgroundColor' : '#3aaab2'}),
+            style = {'padding' : '10px' , 'backgroundColor' : '#3aaab2'}),
 
         html.Div([
             html.Label("Choose a topic "),
@@ -31,7 +41,7 @@ app.layout = html.Div([
                 {'label':'US-China Relations', 'value':'China' },
                 {'label': 'COVID-19', 'value':'covid'},
                 ],
-            value = 'covid')], style = {'width': '20%', 'display': 'inline-block'} ),
+            value = 'covid')], style = {'padding': '5px', 'width': '20%', 'display': 'inline-block'} ),
 
         html.Div([
             html.Label("Choose a model "),
@@ -40,25 +50,34 @@ app.layout = html.Div([
                 {'label':'Linear Regression', 'value':'linear' },
                 {'label': 'Logistic Regression', 'value':'logit'},
                 ],
-            value = 'linear')], style = {'width': '20%', 'display': 'inline-block'} ),
+            value = 'linear')], style = {'padding': '5px', 'width': '20%', 'display': 'inline-block'} ),
 
         html.Div([
             html.Label("Choose a sector "),
             dcc.Dropdown(id='sector_dropdown', options=[])], 
-            style = {'width': "20%", 'display': 'inline-block'}),
+            style = {'padding': '5px', 'width': "20%", 'display': 'inline-block'}),
 
+        html.H3('Testing the model', style = {'padding': '3px'}),
         dcc.Graph(id = 'bar_plot'),
+
+        html.H3('Model fit for the training data', style = {'padding': '3px'}),
+        dcc.Graph(id = 'training_plot'),
 
         dcc.Tabs(id='tabs', value='tabs', parent_className='custom-tabs', children=[
             dcc.Tab(
-                    label='About',
-                    value='what-is', className = 'control-tabs'),
+                    label='Tweets ',
+                    value='tweets', className = 'control-tabs',
+                    style={'width': '30%','padding': '3px', 'fontSize': '18px',
+                    'line-height': '5vh'}),
 
             dcc.Tab(
-                    label='Data',
-                    value='data', className = 'control-tabs'),
+                    label='The Stock Data',
+                    value='stock', className = 'control-tabs',
+                    style={'width': '30%','padding': '3px', 'fontSize': '18px',
+                    'line-height': '5vh'}),
                 ]),
         html.Div(id='tabs-content-classes')
+
 ])
     
 @app.callback(
@@ -113,34 +132,71 @@ def graph_update(topic, model_value, sector_value):
 
 
 @app.callback(Output('tabs-content-classes', 'children'),
-              Input('tabs', 'value'))
+            Input('tabs', 'value'))
 def render_content(tab):
-    if tab == 'what-is':
+    if tab == 'stock':
         return html.Div([
             html.P(
-                'Stock prices are often influenced by the public attitude '
-                'towards the market. Psychological research shows that emotions play a '
-                'significant role in human decision-making. Similarly, behavioral finance research '
-                "shows that financial decisions are influenced by people's emotions and moods."
-            ),
-            html.P(
-                "Twitter is a popular place for users to express their moods and sentiments "
-                "towards a variety of topics. We looked into how the polarity of "
-                "sentiments in tweets on COVID-19 and US-China relations are related to "
-                "the daily movements of stock prices!" )
+                "We collected stock data from all S&P 500 companies and grouped the data "
+                "into 11 different sectors: Information Technology, Health Care, Consumer "
+                "Discretionary, Financials, Communication Services, Industrials, Consumer Staples, "
+                "Energy, Real Estate, Materials, and Utilities.", style = {'fontSize': "16px"} )
         ])
-    elif tab == 'data':
+    elif tab == 'tweets':
         return html.Div([
             html.P(
                 "Tweets were collected from the Twitter Developer API. "
-                "We gathered XXXX amount of tweets related to COVID-19 and "
-                "US-China Relations from DATE1 to DATE2 "
-            ),
+                "We gathered XXXX tweets related to COVID-19 and "
+                "US-China Relations from DATE1 to DATE2. "),
             html.P(
-                "We collected stock data from all S&P 500 companies and grouped the data "
-                "into 11 different sectors: Health Care, Consumer Discretionary, "
-                "Financials, Communication Services, Industrials, Consumer Staples, "
-                "Energy, Real Estate, Materials, and Utilities." )
+                'The query keywords used to collect tweets are as follows: '),
+            html.P(
+                'COVID-19: covid19, covid, covid-19, vaccine, vaccination, omicron, '
+                'booster, mask, mandate, lockdown, death rate, travel restriction, '
+                'total infections, breakthrough infections, social distancing, '
+                'quarantine, isolation, pandemic, & shutdown. '),
+            html.P(
+                'US-China Relations: Trump, Biden, trade war, Xi Jinping, Eileen Gu, '
+                'Taiwan, Hong Kong, CCP, TikTok, Huawei, tariffs, human rights, '
+                'Xinjiang, Zhao Lijian, & Ned Price.'
+            ), 
         ])
+    
+
+@app.callback(Output('training_plot', 'figure'),
+            [Input('topic_dropdown', 'value'),
+            Input('model_dropdown', 'value'),
+            Input('sector_dropdown', 'value')])
+def graph_update_tr(topic, model_value, sector_value):
+    if model_value == 'linear':
+        tr1 = tr[(tr['topic'] == topic) & (tr['industry'] == sector_value) & (tr['model'] == model_value)]
+        fig1 = go.Figure(go.Scatter(x = tr1['date'], y = tr1['true_y'], 
+                        name = 'Actual price using {} model'.format(model_value),
+                        line = {'color': 'firebrick', 'width': 4}))
+
+        fig1.add_trace(go.Scatter(x = tr1['date'], y = tr1['fitted_y'],
+                        name = 'Fitted price {} model'.format(model_value),
+                        line = {'color': 'blue', 'width': 2, 'dash': 'dash'}))
+        
+        fig1.update_layout(title = 'Actual vs. Fitted for {} Sector'.format(sector_value),
+                        xaxis_title = 'Date',
+                        yaxis_title = 'Price'
+                        )
+    else:
+        tr1 = tr[(tr['topic'] == topic) & (tr['model'] == model_value)]
+        tr1['accurate'] = np.where(tr1['fitted_y'] == tr1['true_y'], 1, 0)
+        tr2 = pd.DataFrame()
+        tr2['accuracy'] = tr1.groupby('industry')['accurate'].mean()
+        tr2.reset_index(inplace = True)
+
+        fig1 = go.Figure(go.Bar(x = tr2['industry'], y = tr2['accuracy']
+                        ))
+        fig1.update_layout(title = 'Training accuracy rate using {} model'.format(model_value),
+                        xaxis_title = 'Sector',
+                        yaxis_title = 'Accuracy rate (%)'
+                        )
+
+    return fig1
+
 if __name__ == '__main__': 
     app.run_server()
