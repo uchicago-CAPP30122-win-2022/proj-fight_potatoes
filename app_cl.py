43d8ac80a21c8f934131cdbba90dcc960f1b4aaf
Carolyn Liu <carolynliu@Carolyns-MacBook-Pro.local>
2022-03-12 15:45:08 -0600

    add tab

--- app_cl.py
+++ app_cl.py
@@ -20,17 +20,34 @@ all_options = {
 df = pd.read_csv('fake_output.csv')
 tr = pd.read_csv('fake_training_output.csv')
 
+tabs_styles = {
+    'height': '44px'
+}
+tab_style = {
+    'borderBottom': '1px solid #d6d6d6',
+    'padding': '6px',
+    'fontWeight': 'bold'
+}
+
+tab_selected_style = {
+    'borderTop': '1px solid #d6d6d6',
+    'borderBottom': '1px solid #d6d6d6',
+    'backgroundColor': '#119DFF',
+    'color': 'white',
+    'padding': '6px'
+}
+
 app.layout = html.Div([
         html.Div([
             html.H1("Can Moods on Twitter Predict Stock Price Movements?"),
-            html.P('Stock prices are often influenced by the public attitude '
-                'towards the market. Psychological research shows that emotions play a '
-                'significant role in human decision-making. Similarly, behavioral finance research '
-                "shows that financial decisions are influenced by people's emotions and moods. "),
-            html.P("Twitter is a popular place for users to express their moods and sentiments "
-                "towards a variety of topics. We looked into how the polarity of "
-                "sentiments in tweets on COVID-19 and US-China relations are related to "
-                "the daily movements of stock prices!") 
+            #html.P('Stock prices are often influenced by the public attitude '
+            #    'towards the market. Psychological research shows that emotions play a '
+            #    'significant role in human decision-making. Similarly, behavioral finance research '
+            #    "shows that financial decisions are influenced by people's emotions and moods. "),
+            #html.P("Twitter is a popular place for users to express their moods and sentiments "
+            #    "towards a variety of topics. We looked into how the polarity of "
+            #    "sentiments in tweets on COVID-19 and US-China relations are related to "
+            #    "the daily movements of stock prices!") 
                 ], 
             style = {'padding' : '10px' , 'backgroundColor' : '#3aaab2'}),
 
@@ -63,20 +80,28 @@ app.layout = html.Div([
         html.H3('Model fit for the training data', style = {'padding': '3px'}),
         dcc.Graph(id = 'training_plot'),
 
-        dcc.Tabs(id='tabs', value='tabs', parent_className='custom-tabs', children=[
-            dcc.Tab(
-                    label='Tweets ',
-                    value='tweets', className = 'control-tabs',
-                    style={'width': '30%','padding': '3px', 'fontSize': '18px',
-                    'line-height': '5vh'}),
+        #dcc.Tabs(id='tabs', value='tabs', parent_className='custom-tabs', children=[
+        #    dcc.Tab(
+        #            label='Tweets ',
+        #            value='tweets', className = 'control-tabs',
+        #            style={'width': '30%','padding': '3px', 'fontSize': '18px',
+        #            'line-height': '5vh'}),
 
-            dcc.Tab(
-                    label='The Stock Data',
-                    value='stock', className = 'control-tabs',
-                    style={'width': '30%','padding': '3px', 'fontSize': '18px',
-                    'line-height': '5vh'}),
-                ]),
-        html.Div(id='tabs-content-classes')
+        #    dcc.Tab(
+        #            label='The Stock Data',
+        #            value='stock', className = 'control-tabs',
+        #           style={'width': '30%','padding': '3px', 'fontSize': '18px',
+        #            'line-height': '5vh'}),
+        #       ]),
+
+        dcc.Tabs(id="tabs-styled-with-inline", value='tab-1', children=[
+            dcc.Tab(label='About', value='About', style=tab_style, selected_style=tab_selected_style),
+            dcc.Tab(label='The Tweet Data', value='The Tweet Data', style=tab_style, selected_style=tab_selected_style),
+            dcc.Tab(label='The Stock Data', value='The Stock Data', style=tab_style, selected_style=tab_selected_style),
+            dcc.Tab(label='The Output', value='The Output', style=tab_style, selected_style=tab_selected_style),
+        ], style=tabs_styles), 
+    html.Div(id='tabs-content-inline')
+
 
 ])
     
@@ -130,7 +155,7 @@ def graph_update(topic, model_value, sector_value):
 
     return fig
 
-
+'''
 @app.callback(Output('tabs-content-classes', 'children'),
             Input('tabs', 'value'))
 def render_content(tab):
@@ -161,7 +186,66 @@ def render_content(tab):
                 'Xinjiang, Zhao Lijian, & Ned Price.'
             ), 
         ])
-    
+'''
+@app.callback(Output('tabs-content-inline', 'children'),
+              Input('tabs-styled-with-inline', 'value'))
+def render_content(tab):
+    if tab == 'About':
+        return html.Div([
+            html.H3("""
+            Stock prices are influenced by the public attitude towards 
+            the market. Psychological research that emotions play a significant role 
+            in human decision-making. Behavioral finance showed that financial decisions 
+            are influenced by people's emotions and moods. """),
+            html.H3('''
+            Twitter is a popular place for users to express their moods and sentiments 
+            towards a variety of topics. We look into how the polarity of sentiments in 
+            tweets is related to the daily movements of stock prices!
+            ''')
+        ])
+    elif tab == 'The Tweet Data':
+        return html.Div([
+                    html.H3('''
+                        Tweets were collected from the Twitter Developer API. 
+                        We gathered XXXX tweets related to COVID-19 and 
+                        US-China Relations from DATE1 to DATE2. 
+                        '''),
+                    html.H3(
+                        'The query keywords used to collect tweets are as follows: '),
+                    html.H3('''
+                        COVID-19: covid19, covid, covid-19, vaccine, vaccination, omicron,
+                        booster, mask, mandate, lockdown, death rate, travel restriction,
+                        total infections, breakthrough infections, social distancing,
+                        quarantine, isolation, pandemic, & shutdown. '''),
+                    html.H3(
+                        'US-China Relations: Trump, Biden, trade war, Xi Jinping, Eileen Gu, '
+                        'Taiwan, Hong Kong, CCP, TikTok, Huawei, tariffs, human rights, '
+                        'Xinjiang, Zhao Lijian, & Ned Price.'
+            ), 
+        ])
+    elif tab == 'The Stock Data':
+        return html.Div([
+            html.H3('''
+                We collected stock data from all S&P 500 companies and grouped the data 
+                into 11 different sectors: Information Technology, Health Care, Consumer 
+                Discretionary, Financials, Communication Services, Industrials, Consumer Staples, 
+                Energy, Real Estate, Materials, and Utilities.
+            '''
+            )
+        ])
+    elif tab == 'The Output':
+        return html.Div([
+            html.H3('''
+            You can check out the differences through the various dropdown menus. 
+            For both topics, you may select
+            a model to see the accuracy of the prediction using average Twitter
+            sentiments on stock market movement. 
+            If you choose linear regression, you can choose a sector.
+            For all other models, we show the accuracy of the models with all sectors
+            on one plot.
+            '''
+            )
+        ])
 
 @app.callback(Output('training_plot', 'figure'),
             [Input('topic_dropdown', 'value'),
