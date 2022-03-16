5a9df2c8e7f5ffb0798af578ef261549b3345f96
SiwenSusan <91503894+SiwenSusan@users.noreply.github.com>
2022-03-16 09:02:07 -0500

    Add files via upload

--- analyze_tweets/analyze/modelling_part2.py
+++ analyze_tweets/analyze/modelling_part2.py
@@ -16,32 +16,29 @@ from analyze import modelling_part1
 
 def process_all_model(modelname, dates, topics, y_datafile, stocks, test_date):
     """
-    Process all models and save results
     Input:
-        modelname (list): models to run
-        dates (list): training dates
-        topics (list): TOPIC
-        y_datafile (list): filename/path for stock data
-        stocks (list): STOCK
-        test_date (list): dates for testing data
-    
-    Returns:
-        all_model: dict mapping stock, topic and model to 
-            the results
+        topic: list
+        modelname: list
+        stock: list
+        
     """
+
     print("++++++++++++++++++ starting to prepare training data ++++++++++++++++++")
     y_dic, x_dic, x3_dic = modelling_part1.collect_all_x_and_y(dates, topics, y_datafile, stocks)
 
     print("++++++++++++++++++ starting to prepare testing data +++++++++++++++++++")
     if test_date:
-        y_test, x_dic_test, x3_dic_test = modelling_part1.collect_all_x_and_y(test_date, topics, y_datafile, stocks)
+        y_dic_test, x_dic_test, x3_dic_test = modelling_part1.collect_all_x_and_y(test_date, topics, y_datafile, stocks)
     else:
         testing_data = None
     
-    
+    # print(f'x3_dic is {x3_dic}')
+    # print(f'x_dic is {x_dic}')
+    # print(f'x3_dic_test is {x3_dic_test}')
+    # print(f'x_dic_test is {x_dic_test}')
     print("++++++++++++++++++++ starting to train models ++++++++++++++++++++++++++")
     all_model = {}
-
+    
     selection_on_x = list(input("Now you can select from polarity(0), subjectivity(1), moving average(2) as the regressors, example: [0, 1] (or just press return button if you want them all) : ") \
         or [0,1,2])
 
@@ -49,14 +46,14 @@ def process_all_model(modelname, dates, topics, y_datafile, stocks, test_date):
         for stock_name in stocks:
             for name in modelname:
                 training_x = select_on_xs(selection_on_x, x_dic[topic_name], x3_dic[stock_name])
-                training_data = y_dic[stock_name] + (training_x, )
+                training_data = y_dic[stock_name] + (training_x,)
                 if test_date:
                     training_x_test = select_on_xs(selection_on_x, x_dic_test[topic_name], x3_dic_test[stock_name])
-                    testing_data = y_test[stock_name] + (training_x_test, )
-                all_model[f"{stock_name}_{topic_name}_{name}"] = TrainingModel(name, 
-                    dates, stock_name, topic_name, training_data, testing_data, test_date)
+                    testing_data = y_dic_test[stock_name] + (training_x_test,)
+                all_model[f"{stock_name}_{topic_name}_{name}"] = TrainingModel(name, dates, stock_name, topic_name, training_data, testing_data, test_date)
         
     return all_model
+    
 
 ######################### FINISH DOC STRING #############################
 def select_on_xs(selection_on_x, x1_x2, x3):
@@ -70,16 +67,20 @@ def select_on_xs(selection_on_x, x1_x2, x3):
 
     Returns: 
     """
+    # print(f"x1_x2 is {x1_x2}")
+    # print(f"x1_x2[0] is {x1_x2[0]}")
+    # print(f"x1_x2[0] is {x1_x2[:,[0]]}")
+
     training_x = []
     if 0 in selection_on_x:
-        training_x.append(x1_x2[0][0])
+        training_x.append(x1_x2[:,0])
 
     if 1 in selection_on_x:
-        training_x.append(x1_x2[0][1])
+        training_x.append(x1_x2[:,1])
 
     if 2 in selection_on_x:
         training_x.append(x3)
-
+    print(f"need transpose, {training_x}")
     return np.array(training_x).transpose()
 
 
@@ -122,9 +123,10 @@ class TrainingModel:
         self.test_date = test_date
         self.prediction = None
         self.test_accuracy = None
-        self.true_testing_y = None            
+        self.true_testing_y = None  
+        self.R2 = None          
         self.model = self.model_building(model_name, testing_data)
-        self.R2 = sklearn.metrics.r2_score(self.true_y, self.fitted_value)
+             
         
 
     def model_building(self, model_name, test):
@@ -141,7 +143,7 @@ class TrainingModel:
         y_dummy, y_num, Xs = self.training_data
 
         if model_name == "linear":
-            print(Xs)
+            print("Xs is :",Xs)
             X2 = sm.add_constant(Xs)
             model_1 = sm.OLS(y_num, X2)
             model = model_1.fit()
@@ -153,9 +155,11 @@ class TrainingModel:
                 self.prediction = model.predict(update_test_x)
                 self.true_testing_y = self.testing_data[1]
                 self.test_accuracy = "Not applicable to linear model"
+                self.R2 = sklearn.metrics.r2_score(self.true_y, self.fitted_value)
 
-        else:
+        elif len(np.unique(np.array(y_dummy))) > 1:
             if model_name == "logistic":
+                print(f"Xs is {Xs}, y_dummy is {y_dummy}")
                 logreg = LogisticRegression(random_state=42)
                 model = logreg.fit(Xs, y_dummy)
 
@@ -173,5 +177,8 @@ class TrainingModel:
                 self.prediction = model.predict(self.testing_data[2])
                 self.true_testing_y = self.testing_data[0]
                 self.test_accuracy = accuracy_score(self.true_testing_y, self.prediction)
-
+                self.R2 = sklearn.metrics.r2_score(self.true_y, self.fitted_value)
+        else:
+            print("y_dummy is not varying so only linear regression model is provided")
+            model = None
         return model
