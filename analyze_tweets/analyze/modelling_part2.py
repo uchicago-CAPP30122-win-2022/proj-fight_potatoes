ed61ab16753e16e814bf3cb46a4422934be6b9fb
SiwenSusan <91503894+SiwenSusan@users.noreply.github.com>
2022-03-16 16:11:22 -0500

    Add files via upload

--- analyze_tweets/analyze/modelling_part2.py
+++ analyze_tweets/analyze/modelling_part2.py
@@ -16,60 +16,68 @@ from analyze import modelling_part1
 
 def process_all_model(modelname, dates, topics, y_datafile, stocks, test_date):
     """
+    to generate a dictionary that contains all different models
+
     Input:
-        topic: list
-        modelname: list
-        stock: list
-        
+        modelname: list of string, eg:["KNN","linear"]
+        dates: list of int, training dates, eg: [213, 214, 215]
+        topics: list of string, eg: ["china","covid"]
+        y_datafile: string, the filename of the file which stores all dependent variable
+        stocks: list of string, eg: ["SPY", "Health Care"]. Note:"SPY" indicates SP500
+        test_date: list of int, should contain at least two testing dates, eg: [314, 315]
+    
+    Return:
+        A dictionary containing all TrainingModel
     """
 
     print("++++++++++++++++++ starting to prepare training data ++++++++++++++++++")
+
     y_dic, x_dic, x3_dic = modelling_part1.collect_all_x_and_y(dates, topics, y_datafile, stocks)
 
     print("++++++++++++++++++ starting to prepare testing data +++++++++++++++++++")
     if test_date:
-        y_dic_test, x_dic_test, x3_dic_test = modelling_part1.collect_all_x_and_y(test_date, topics, y_datafile, stocks)
+        y_dic_test, x_dic_test, x3_dic_test = \
+            modelling_part1.collect_all_x_and_y(test_date, topics, y_datafile, stocks)
     else:
         testing_data = None
     
     print("++++++++++++++++++++ starting to train models ++++++++++++++++++++++++++")
     all_model = {}
     
-    selection_on_x = input("Now you can select from polarity(0), subjectivity(1), moving average(2) as the regressors, example: 0 1 (or just press return button if you want them all) : ") \
-        or [0,1,2]
+    selection_on_x = input("""Now you can select from polarity(0), subjectivity(1), 
+    moving average(2) as the regressors, example: 0 1 (or just press return button 
+    if you want them all) : """) or [0,1,2]
+    
     if type(selection_on_x) is not list:
         selection_on_x =  selection_on_x.split()
         selection_on_x = [int(x) for x in selection_on_x ]
-
     for topic_name in topics:
         for stock_name in stocks:
             for name in modelname:
                 training_x = select_on_xs(selection_on_x, x_dic[topic_name], x3_dic[stock_name])
                 training_data = y_dic[stock_name] + (training_x,)
                 if test_date:
-                    training_x_test = select_on_xs(selection_on_x, x_dic_test[topic_name], x3_dic_test[stock_name])
+                    training_x_test = select_on_xs(selection_on_x, x_dic_test[topic_name], \
+                        x3_dic_test[stock_name])
                     testing_data = y_dic_test[stock_name] + (training_x_test,)
                 all_model[f"{stock_name}_{topic_name}_{name}"] = TrainingModel(name, dates, stock_name, topic_name, training_data, testing_data, test_date)
-        
+
     return all_model
     
 
-######################### FINISH DOC STRING #############################
 def select_on_xs(selection_on_x, x1_x2, x3):
     """
     Select independent variables
 
     Inputs:
-        selection_on_x:
-        x1_x2:
-        x3:
+        selection_on_x: list of string, [0,1,2] means we want all 3 variables 
+            to train the model
+        x1_x2: np.array which contains x1 and x2 
+        x3: a np array which contains x3
 
     Returns: 
+        a matrix containing the chosen Xs
     """
-    print(f"x1_x2 is {x1_x2}")
-    #print(f"x1_x2[0] is {x1_x2[0]}")
-    #print(f"x1_x2[0] is {x1_x2[:,0]}")
-    print(type(x1_x2))
 
     training_x = []
     if 0 in selection_on_x:
@@ -80,7 +88,7 @@ def select_on_xs(selection_on_x, x1_x2, x3):
 
     if 2 in selection_on_x:
         training_x.append(x3)
-    print(f"need transpose, {training_x}")
+
     return np.array(training_x).transpose()
 
 
@@ -105,7 +113,7 @@ class TrainingModel:
         R2: R^2
 
     Methods:
-        model_building (self):
+        model_building (self, model_name, test):
     """
     
     def __init__(self, model_name, dates, stock, topic, training_data, testing_data = None, test_date = None):
@@ -138,12 +146,11 @@ class TrainingModel:
             test: (bool) whether to use test data to test
 
         Returns:
-            model: instance of the TrainingModel class
+            model: a model
         """
         y_dummy, y_num, Xs = self.training_data
 
         if model_name == "linear":
-            print("Xs is :",Xs)
             X2 = sm.add_constant(Xs)
             model_1 = sm.OLS(y_num, X2)
             model = model_1.fit()
@@ -159,7 +166,6 @@ class TrainingModel:
 
         elif len(np.unique(np.array(y_dummy))) > 1:
             if model_name == "logistic":
-                print(f"Xs is {Xs}, y_dummy is {y_dummy}")
                 logreg = LogisticRegression(random_state=42)
                 model = logreg.fit(Xs, y_dummy)
 
@@ -179,6 +185,7 @@ class TrainingModel:
                 self.test_accuracy = accuracy_score(self.true_testing_y, self.prediction)
                 self.R2 = sklearn.metrics.r2_score(self.true_y, self.fitted_value)
         else:
-            print("y_dummy is not varying so only linear regression model is provided")
+            # in case if y are all 0 or all 1, then we cannot train a model
+            # when y has no variation 
             model = None
         return model
